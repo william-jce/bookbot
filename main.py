e3139d23f7af4e73ba9288ab17e42644de6f55b1
william-jce <william.entschluss@gmail.com>
2025-08-28 15:28:50 +0900

    add analysis helper and cli input

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+import sys
 from stats import num_words
 from stats import num_char
+from stats import report
 
 def main():
-  frankenstein = get_book_text("./books/frankenstein.txt")
-  print(f"{num_words(frankenstein)} words found in the document")
-  frankenstein_char = num_char(frankenstein)
-  print(frankenstein_char)
+  if len(sys.argv) != 2:
+    print(f"Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+  #book_text = get_book_text(sys.argv[1])
+  #book_char = num_char(book_text)
+  print("============ BOOKBOT ============")
+  print(f"Analyzing book found at {sys.argv[1]}")
+  print("----------- Word Count ----------")
+  print(f"Found {num_words(get_book_text(sys.argv[1]))} total words")
+  print("--------- Character Count -------")
+  for char in report(num_char(get_book_text(sys.argv[1]))):
+    print(f"{char["char"]}: {char["num"]}")
 
 def get_book_text(filepath):
   with open(filepath) as f:
--- stats.py
+++ stats.py
@@ -9,3 +9,14 @@ def num_char(book_text):
       char_count[char] = 0
     char_count[char] += 1
   return char_count
+
+def sort_enable(items):
+  return items["num"]
+
+def report(char_dict):
+  sorted_dicts = []
+  for char in char_dict:
+    if char.isalpha():
+      sorted_dicts.append({"char": char, "num": char_dict[char]})
+  sorted_dicts.sort(reverse=True, key=sort_enable)
+  return sorted_dicts
